b98104ed04dc6639b2af6d323ee9e2879294b698
asmaa_alshareef <81930013+asma121@users.noreply.github.com>
2021-12-06 21:21:56 +0300

    .

--- ColdCall v.2/ViewController.swift
+++ ColdCall v.2/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    let names = ["Deem","Layan","Maha","Noor","Leen","Hajar","Linah","Asmaa"]
+    @IBAction func coldCallButton(_ sender: Any) {
+        let shuffledNames = names.shuffled()
+        nameLabel.text = shuffledNames[0]
+        updateNumberLabel()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    func updateNumberLabel(){
+        let number = Int.random(in: 1...5)
+        numberLabel.text = String(number)
+        
+        if number == 1 || number == 2{
+            numberLabel.textColor = .red
+        }else if number == 3 || number == 4 {
+            numberLabel.textColor = .orange
+        }else{
+            numberLabel.textColor = .green
+        }
+    }
 
 }
 
